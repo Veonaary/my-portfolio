c6111e99055f98777f2657f23023eedf99f55195
Hafiz Mustafa Amin <veonary24@gmail.com>
2025-06-09 22:12:30 +0500

    adding other things

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+const cursor = document.createElement("div");
+cursor.id = "custom-cursor";
+document.body.appendChild(cursor);
+
+document.addEventListener("mousemove", e => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
+
